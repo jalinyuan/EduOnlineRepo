372b7c82083930dbd65ca14293f5888f0218e269
yuanzhonglin <497259869@qq.com>
2015-09-06 09:25:43 +0800

    初始化项目

---
+++ EduOnline/src/com/edu/index/IndexController.java
@@ -0,0 +1,13 @@
+package com.edu.index;
+
+import com.jfinal.core.Controller;
+
+/**
+ * IndexController
+ */
+public class IndexController extends Controller {
+	public void index() {
+		render("index.html");
+	}
+}
+
